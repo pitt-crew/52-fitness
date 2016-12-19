7d53f139f881c8baa9bb64842c39fad7f23eca7f
Mike DeAngelis <mjdeangelis@gmail.com>
2016-12-18 21:46:45 -0600

    Added font vector icons and some work on login screen UI

--- LoginPage.js
+++ LoginPage.js
@@ -8,9 +8,12 @@ import {
   View,
   TouchableHighlight,
   ActivityIndicator,
-  Image
+  Image,
+  Button
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
+import AppButton from './app/components/Button';
 /*Add any other components that we need on this screen here*/
 
 var styles = StyleSheet.create({
@@ -23,7 +26,30 @@ var styles = StyleSheet.create({
   container: {
     padding: 30,
     marginTop: 65,
-    alignItems: 'center'
+    alignItems: 'center',
+    // backgroundColor: 'black',
+    flex: 1,
+    flexDirection: 'column'
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: '#6c6d6f',
+
+  },
+  inputIcon: {
+    marginTop: 15,
+    color: '#6c6d6f'
+  },
+  textInput: {
+    padding: 4,
+    height: 30,
+    marginTop: 5,
+    marginRight: 5,
+    flex: 1,
+    width: 20,
+    fontSize: 18,
+    color: '#000'
   }
 });
 
@@ -32,8 +58,34 @@ class LoginPage extends Component {
     	return (
 	      <View style={styles.container}>
 	        <Text style={styles.description}>
-	          This is a login page.
+	          Welcome
 	        </Text>
+          <Text style={styles.description}>
+            to 52 Fitness
+          </Text>
+          <View style={styles.inputContainer}>
+            <Icon style={styles.inputIcon} name="user-circle" />
+            <TextInput
+              style={styles.textInput}
+              autoCorrect="false"
+              placeholder='Username' />
+          </View>
+          <View style={styles.inputContainer}>
+            <Icon style={styles.inputIcon} name="lock" />
+            <TextInput
+              style={styles.textInput}
+              secureTextEntry="true"
+              placeholder='Password' />
+          </View>
+          <View>  
+            <Button 
+              title="Login"
+              color="#000"
+              accessibilityLabel="Learn more about this purple button" />
+          </View>
+          <View>
+            <Text>Or Register Here</Text>
+          </View>
 	        
 	      </View>	
     	);
--- index.ios.js
+++ index.ios.js
@@ -8,6 +8,7 @@
 var React = require('react');
 var ReactNative = require('react-native');
 var LoginPage = require('./LoginPage');
+
 /*Add any other components that we need on this screen here*/
 
 var styles = ReactNative.StyleSheet.create({
